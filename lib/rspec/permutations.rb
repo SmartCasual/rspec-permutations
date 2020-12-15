6a9481d324252b9cb99b2dd4d502e79a53b2af28
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2020-12-15 18:09:32 +0000

    Generated gem

---
+++ lib/rspec/permutations.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "permutations/version"
+
+module Rspec
+  module Permutations
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
